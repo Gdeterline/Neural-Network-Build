90e8358ee14b21296d2ce9890a71faea350af762
Guillaume de Terline <guillaume.macquart_de_terline@telecom-sudparis.eu>
2024-11-08 16:31:55 +0100

    started fixing feed_forward() func - still under testing

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ import numpy as np
 
 def init_layers(nb_layers, nb_neurons):
     
+    if type(nb_layers) != int:
+        raise Exception("Number of layers must be integer")
+    if len(nb_neurons) != nb_layers:
+        raise Exception("The nb_neurons is per layer - The nb_neurons list length must match nb_layers")
+    if type(nb_neurons) != list:
+        raise Exception("nb_neurons parameter is supposed to be a list")
+
     W = []
-    b = []
+    b = [0]
     
     for i in range(0, nb_layers-1):
         W.append(np.random.randn(nb_neurons[i+1], nb_neurons[i]))
@@ -50,10 +57,11 @@ def feed_forward_test(nb_layers, X, W, b, g=sigmoid):
     # Initialize the input layer
     A = [0 for i in range(nb_layers)]
     A[0] = X.T
-    Z = [0 for i in range(nb_layers)]
+    Z = [0 for i in range(0, nb_layers)]
+    Z[0] = A[0]
     
     # Neuron values updated with the forward pass
-    for i in range(1, len(W)):
+    for i in range(1, len(W)+1):
         Z[i] = W[i-1] @ A[i-1] + b[i-1]
         A[i] = g(Z[i])
     return A, Z, A[-1]
@@ -62,23 +70,36 @@ def feed_forward_test(nb_layers, X, W, b, g=sigmoid):
 def feed_forward(nb_layers, nb_neurons, X, g=sigmoid):
     
     
+    
     # Initialize the input layer
     A = [0 for i in range(nb_layers)]
     A[0] = X.T
-    Z = [0 for i in range(nb_layers)]
+    Z = [0 for i in range(0, nb_layers)]
+    Z[0] = A[0]
     W, b = init_layers(nb_layers, nb_neurons)
 
     print(W)
     print("----------------")
     print(b)
+    print(len(b))
     print("----------------")
     
     # Neuron values updated with the forward pass
     for i in range(1, len(W)):
-        Z[i] = W[i-1] @ A[i-1] + b[i-1]
+        Z[i] = W[i-1] @ A[i-1] + b[i]
         A[i] = g(Z[i])
     return A, Z, A[-1]
 
 
 #print(feed_forward(3, [2, 2, 1], np.array([[1, 2], [3, 4]]), g=sigmoid))
-init_layers(3, [3, 2, 2])
+
+W = [np.array([[1.], [2.]]), np.array([[1., 2.]])]
+b = [np.array([[1.], [2.]]), np.array([[3.]])]
+
+L=3
+    # n = [1, 2, 1] number of neurons
+X = np.array([[1.]])
+    
+A, Z, output = feed_forward_test(L, X, W, b)
+
+print(Z)

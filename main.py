5ac7c175a1ce17426d842d32bf82d2754d54e1b4
Guillaume de Terline <guillaume.macquart_de_terline@telecom-sudparis.eu>
2024-11-22 15:05:26 +0100

    Solved feed_forward function testing issue
    Mismatch between the two W defined in the testing cell. Function was good.

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def softmax(x):
 # nb_neurons of the input layer must match the number of features
 # g refers to the activation function. Default is sigmoid
 
-def feed_forward_test(nb_layers, X, W, b, g=sigmoid):
+def feed_forward(nb_layers, X, W, b, g=sigmoid):
     
     # Initialize the input layer
     A = [0 for i in range(nb_layers)]
@@ -61,50 +61,8 @@ def feed_forward_test(nb_layers, X, W, b, g=sigmoid):
     Z[0] = A[0]
     
     # Neuron values updated with the forward pass
-    for i in range(1, len(W)+1):
+    for i in range(1, nb_layers):
         Z[i] = W[i-1] @ A[i-1] + b[i]
         A[i] = g(Z[i])
     return A, Z, A[-1]
-
-
-def feed_forward(nb_layers, nb_neurons, X, g=sigmoid):
-    
-    # Initialize the input layer
-    A = [0 for i in range(nb_layers)]
-    A[0] = X
-    Z = [0 for i in range(0, nb_layers)]
-    Z[0] = A[0]
-    W, b = init_layers(nb_layers, nb_neurons)
-
-    #print(W)
-    #print("----------------")
-    #print(b)
-    #print(len(b))
-    #print("----------------")
-    
-    # Neuron values updated with the forward pass
-    for i in range(1, len(W)+1):
-        Z[i] = W[i-1] @ A[i-1] + b[i]       # Need to check this line because the value on the right is correct but not the one on the left
-        A[i] = g(Z[i])
-    return A, Z, A[-1]
-
-
-W, b = init_layers(3, [3, 2, 2])
-
-print(W)
-print(b)
-
-
-""" #print(feed_forward(3, [2, 2, 1], np.array([[1, 2], [3, 4]]), g=sigmoid))
-
-W = [np.array([[1.], [2.]]), np.array([[1., 2.]])]
-b = [np.array([[1.], [2.]]), np.array([[3.]])]
-
-L=3
-    # n = [1, 2, 1] number of neurons
-X = np.array([[1.]])
-    
-A, Z, output = feed_forward_test(L, X, W, b)
-
-print(Z) """
 
